0f8d1187c61f3367509ebd9776ebcaa2fa6dd914
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-05-14 16:34:50 +0600

    added skill section

--- src/components/Body/Skills/Skills.js
+++ src/components/Body/Skills/Skills.js
@@ -1,10 +1,33 @@
 import React from 'react';
-
+import Separator from '../../Common/separater/Separator';
+import { SkillsData } from '../../Data/skills';
+import SkillCard from './SkillCard/SkillCard';
+import './Skills.css'
 const Skills = () => {
+    const data = SkillsData;
     return (
-        <div>
-            <h2>This is skills</h2>
-        </div>
+        <div className='skills'>
+            <Separator></Separator>
+            <label className='section-title'>Skills</label>
+            <div className='skills-container'>
+                {
+                    data.map((item) => {
+                        return (
+                            <div className='skills-section'>
+                                <label className='skills-section-title' >{item.type}</label>
+                                <div className='skills-list'>
+                                    {item.list.map((skill) => {
+                                        return (
+                                            <SkillCard skill={skill}></SkillCard>
+                                        )
+                                    })}
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div >
     );
 };
 
---
+++ src/components/Data/skills.js
@@ -0,0 +1,45 @@
+export const SkillsData = [
+    {
+        type: "Frontend",
+        list: [
+            {
+                name: "ReactJS",
+                icon: <i class="devicon-react-original colored"></i>
+            },
+            {
+                name: "ReactJS",
+                icon: <i class="devicon-react-original colored"></i>
+            },
+            {
+                name: "ReactJS",
+                icon: <i class="devicon-react-original colored"></i>
+            },
+            {
+                name: "ReactJS",
+                icon: <i class="devicon-react-original colored"></i>
+            },
+        ]
+    },
+    {
+        type: "Backend",
+        list: [
+            {
+                name: "Python",
+                icon: <i class="devicon-python-plain colored"></i>
+
+            },
+            {
+                name: "JAVA",
+                icon: <i class="devicon-java-plain colored"></i>
+            },
+            {
+                name: "ReactJS",
+                icon: <i class="devicon-react-original colored"></i>
+            },
+            {
+                name: "ReactJS",
+                icon: <i class="devicon-react-original colored"></i>
+            },
+        ]
+    }
+]
