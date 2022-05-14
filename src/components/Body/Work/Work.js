447f41d1e14f5a9483015782812c9db60e0c63fc
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-05-14 17:07:38 +0600

    added work section

--- src/components/Body/Work/Work.js
+++ src/components/Body/Work/Work.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import Separator from '../../Common/separater/Separator';
+import { WorkData } from '../../Data/work';
+import './Work.css';
+import WorkCard from './WorkCard/WorkCard';
+
 
 const Work = () => {
+    const data = WorkData;
     return (
-        <div>
-            <h2>This is work</h2>
+        <div className='work'>
+            <Separator></Separator>
+            <label className='section-title'>Work</label>
+            <div className='work-list'>
+                {
+                    data.map((item) => {
+                        return (
+                            <WorkCard item={item}></WorkCard>
+                        )
+                    })
+                }
+            </div>
+
         </div>
     );
 };
---
+++ src/components/Data/work.js
@@ -0,0 +1,18 @@
+export const WorkData = [
+    {
+        company: 'InstantKaj.com',
+        designation: "Software Engineer",
+        dateJoining: "01-10-2020",
+        dateEnd: "Present",
+        comapnyLogo: "https://i.ibb.co/TtbpBkw/ik.png",
+        work: "I have been a software engineer at instantkaj.com and worked on developing frontend and backend of the company."
+    },
+    {
+        company: 'Brac University',
+        designation: "Student Tutor",
+        dateJoining: "01-02-2021",
+        dateEnd: "01-01-2022",
+        comapnyLogo: "https://www.bracu.ac.bd/sites/default/files/resources/media/bracu_logo.png",
+        work: "I have been student tutor at Brac University in CSE220&221( Data structure and Algorithms) course for almost one year."
+    },
+]
