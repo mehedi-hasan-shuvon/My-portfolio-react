918f030ca3f1dd881dca21dd95ee62c46837ac7c
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-05-14 17:28:39 +0600

    added contact me section

--- src/components/Body/Contact/Contact.js
+++ src/components/Body/Contact/Contact.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import Separator from '../../Common/separater/Separator';
+import SocialContact from '../../Common/SocialContact/SocialContact';
+import cv from '../../../assets/cv.pdf'
+import './Contact.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
 
 const Contact = () => {
     return (
-        <div>
-            <h2>This is contact</h2>
+        <div className='contact'>
+            <Separator></Separator>
+            <label className='section-title'>Contact</label>
+            <div className='contact-container'>
+                <div className='contact-left'>
+                    <p>Want to get in Touch, Contact me on any of the platform</p>
+                    <SocialContact></SocialContact>
+                </div>
+                <div className='download'>
+                    <a download href={cv}>
+                        <FontAwesomeIcon icon={faDownload} className='download-icon' />
+                        Download Resume
+                    </a>
+                </div>
+            </div>
         </div>
     );
 };
