a53c2a74479cc0a57e815b33229b0978fb3f9164
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-06-09 23:22:48 +0600

    added partical componenet

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,9 +4,108 @@ import Body from '../Body/Body';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import './Home.css';
+
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
 const Home = () => {
+    const particlesInit = async (main) => {
+        console.log(main);
+
+        // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        await loadFull(main);
+    };
+
+    const particlesLoaded = (container) => {
+        console.log(container);
+    };
     return (
         <div className='home'>
+            <Particles className='partical-class'
+                id="tsparticles"
+                init={particlesInit}
+                loaded={particlesLoaded}
+                options={{
+                    background: {
+                        color: {
+                            value: "#FFFFFF",
+                        },
+                    },
+
+                    fpsLimit: 60,
+                    interactivity: {
+                        events: {
+                            onClick: {
+                                enable: true,
+                                mode: "push",
+                            },
+                            onHover: {
+                                enable: true,
+                                mode: "repulse",
+                            },
+                            resize: true,
+                        },
+                        modes: {
+                            bubble: {
+                                distance: 400,
+                                duration: 2,
+                                opacity: 0.8,
+                                size: 40,
+                            },
+                            push: {
+                                quantity: 4,
+                            },
+                            repulse: {
+                                distance: 100,
+                                duration: 0.4,
+                            },
+                        },
+                    },
+                    particles: {
+                        color: {
+                            value: "#7FFFD4",
+                        },
+                        links: {
+                            color: "#000000",
+                            distance: 150,
+                            enable: true,
+                            opacity: 0.2,
+                            width: 1,
+                        },
+                        collisions: {
+                            enable: true,
+                        },
+                        move: {
+                            direction: "none",
+                            enable: true,
+                            outMode: "bounce",
+                            random: false,
+                            speed: .5,
+                            straight: false,
+                        },
+                        number: {
+                            density: {
+                                enable: true,
+                                value_area: 800,
+                            },
+                            value: 100,
+                        },
+                        opacity: {
+                            value: 0.5,
+                        },
+                        shape: {
+                            type: "circle",
+                        },
+                        size: {
+                            random: true,
+                            value: 5,
+                        },
+                    },
+                    detectRetina: true,
+                }}
+            />
             <Header></Header>
             <Body></Body>
             {/* <Footer></Footer> */}
